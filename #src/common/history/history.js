e91a579845dfff1b201447a60597a29beb6e8384
Vitalii Radaiev <hf.chelenger@gamil.com>
2022-01-14 14:35:38 +0200

    fix

--- #src/common/history/history.js
+++ #src/common/history/history.js
@@ -2,10 +2,10 @@
     let yearsSlider = document.querySelector('.history__years');
     let historySlider = document.querySelector('.history__slider');
 
-    if(yearsSlider && historySlider) {
+    if (yearsSlider && historySlider) {
         let yearItems = document.querySelectorAll('.history__years-item');
         yearItems.forEach((item, index) => item.setAttribute('data-id', index));
-        let el = document.querySelector(`.history__years-item[data-id="${Math.floor(yearItems.length / 2) }"]`);
+        let el = document.querySelector(`.history__years-item[data-id="${Math.floor(yearItems.length / 2)}"]`);
         el.classList.add('_active');
 
         setTimeout(() => {
@@ -19,50 +19,55 @@
                 speed: 800,
                 freeMode: true,
                 initialSlide: Math.floor(yearItems.length / 2),
-               // watchOverflow: true,
+                // watchOverflow: true,
                 centeredSlides: true,
                 //loop: true,
                 //watchSlidesVisibility: true,
-               // slideToClickedSlide: true,
+                // slideToClickedSlide: true,
                 on: {
                     click: async (data, e) => {
                         let item = e.target.closest('.history__years-item');
-                        if(item) {
+                        if (item) {
                             let id = item.dataset.id;
                             item.classList.add('_active');
-                        
+
                             data.slides.forEach(i => {
-                                if(i == item) return;
-        
+                                if (i == item) return;
+
                                 i.classList.remove('_active');
                             })
-    
+
                             await new Promise((res, rej) => {
                                 setTimeout(() => {
                                     dataYearsSlider.update();
                                     res();
-                                },350)
+                                }, 350)
                             })
-    
+
                             dataYearsSlider.slideTo(id);
                             dataHistorySlider.slideTo(id);
                         }
-                    },
-                }
+                    }
+                },
+                // navigation: {
+                //     nextEl: historySlider.querySelector('.slider-button.next'),
+                //     prevEl: historySlider.querySelector('.slider-button.prev'),
+                // },
             });
-    
+
             setTimeout(() => {
                 dataYearsSlider.update();
-            },350)
-    
+            }, 350)
+
             dataHistorySlider = new Swiper(historySlider, {
+                effect: "coverflow",
                 observer: true,
                 observeParents: true,
+                centeredSlides: true,
                 slidesPerView: 1,
-                spaceBetween: 30,
+                spaceBetween: 0,
                 initialSlide: Math.floor(yearItems.length / 2),
                 speed: 800,
-                //loop: true,
                 autoHeight: true,
                 touchRatio: 0,
                 //preloadImages: false,
@@ -71,8 +76,6 @@
                 },
             });
         }, 350)
-
-
 
     }
 }
--- dist/js/app.js
+++ dist/js/app.js
@@ -1403,10 +1403,10 @@ VideoHandler();;
     let yearsSlider = document.querySelector('.history__years');
     let historySlider = document.querySelector('.history__slider');
 
-    if(yearsSlider && historySlider) {
+    if (yearsSlider && historySlider) {
         let yearItems = document.querySelectorAll('.history__years-item');
         yearItems.forEach((item, index) => item.setAttribute('data-id', index));
-        let el = document.querySelector(`.history__years-item[data-id="${Math.floor(yearItems.length / 2) }"]`);
+        let el = document.querySelector(`.history__years-item[data-id="${Math.floor(yearItems.length / 2)}"]`);
         el.classList.add('_active');
 
         setTimeout(() => {
@@ -1420,50 +1420,55 @@ VideoHandler();;
                 speed: 800,
                 freeMode: true,
                 initialSlide: Math.floor(yearItems.length / 2),
-               // watchOverflow: true,
+                // watchOverflow: true,
                 centeredSlides: true,
                 //loop: true,
                 //watchSlidesVisibility: true,
-               // slideToClickedSlide: true,
+                // slideToClickedSlide: true,
                 on: {
                     click: async (data, e) => {
                         let item = e.target.closest('.history__years-item');
-                        if(item) {
+                        if (item) {
                             let id = item.dataset.id;
                             item.classList.add('_active');
-                        
+
                             data.slides.forEach(i => {
-                                if(i == item) return;
-        
+                                if (i == item) return;
+
                                 i.classList.remove('_active');
                             })
-    
+
                             await new Promise((res, rej) => {
                                 setTimeout(() => {
                                     dataYearsSlider.update();
                                     res();
-                                },350)
+                                }, 350)
                             })
-    
+
                             dataYearsSlider.slideTo(id);
                             dataHistorySlider.slideTo(id);
                         }
-                    },
-                }
+                    }
+                },
+                // navigation: {
+                //     nextEl: historySlider.querySelector('.slider-button.next'),
+                //     prevEl: historySlider.querySelector('.slider-button.prev'),
+                // },
             });
-    
+
             setTimeout(() => {
                 dataYearsSlider.update();
-            },350)
-    
+            }, 350)
+
             dataHistorySlider = new Swiper(historySlider, {
+                effect: "coverflow",
                 observer: true,
                 observeParents: true,
+                centeredSlides: true,
                 slidesPerView: 1,
-                spaceBetween: 30,
+                spaceBetween: 0,
                 initialSlide: Math.floor(yearItems.length / 2),
                 speed: 800,
-                //loop: true,
                 autoHeight: true,
                 touchRatio: 0,
                 //preloadImages: false,
@@ -1472,8 +1477,6 @@ VideoHandler();;
                 },
             });
         }, 350)
-
-
 
     }
 };
